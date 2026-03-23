77b5eb14df49d1d07b0036af52cc84cf41583c66
Corentin Mouscio <mouss@MacBook-Air-de-Corentin.local>
2026-03-23 09:51:46 -0400

    Restore BRAIN_ADMIN_KEY verification

--- middleware.ts
+++ middleware.ts
@@ -30,7 +30,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    "/((?!_next|api/brain/|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api(?!/brain)|trpc)(.*)",
   ],
 };
